644732cebac39e8bcb984cd249fe1f17cc568347
Nicodemus <nbezuayeho@gmail.com>
2024-10-25 21:13:50 -0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Nicodemus Bezuahyeho on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
